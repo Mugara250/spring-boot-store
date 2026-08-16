2236938a4ba76cdcb10c4db49fb2f2a402ef0cdc
Mugara250 <mbonyumugara52@gmail.com>
2026-08-16 19:33:19 +0200

    setup

---
+++ src/test/java/com/springboot/springbootstore/SpringBootStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
