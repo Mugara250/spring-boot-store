d99dcb65a2ee6e393fe5c6e6f5d969d8d2decbe8
Mugara250 <mbonyumugara52@gmail.com>
2026-08-17 19:16:46 +0200

    configuring beans using code

--- src/main/java/com/springboot/springbootstore/AppConfig.java
+++ src/main/java/com/springboot/springbootstore/AppConfig.java
@@ -1,10 +1,14 @@
 package com.springboot.springbootstore;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Value("${payment.gateway}")
+    private String paymentGateway;
 
     @Bean
     public StripePaymentService stripePaymentService() {
@@ -18,6 +22,6 @@ public class AppConfig {
 
     @Bean
     public OrdersService ordersService() {
-        return new OrdersService(stripePaymentService());
+        return paymentGateway.equalsIgnoreCase("stripe") ? new OrdersService(stripePaymentService()) : new OrdersService(payPalPaymentService());
     }
 }
