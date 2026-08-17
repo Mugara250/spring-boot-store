65cfe7e00730415cfdddeddb44ca228b4ac902e6
Mugara250 <mbonyumugara52@gmail.com>
2026-08-17 09:19:15 +0200

    dependency injection

--- src/main/java/com/springboot/springbootstore/OrdersService.java
+++ src/main/java/com/springboot/springbootstore/OrdersService.java
@@ -1,7 +1,7 @@
 package com.springboot.springbootstore;
 
 public class OrdersService {
-    public void processOrder() {
+    public void placeOrder() {
         var payment = new StripePaymentService();
         payment.processPayment(100);
     }
